ae5eb15c348466463840e14b0a09288cbf66a68e
jubasky <marcosmiguelrosa@gmail.com>
2016-09-09 04:15:39 +0100

    Revert "initial project version"
    This reverts commit 8f6bf33a930adef541084e452516a4188cfc4146.

--- classGrid.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'm'
-
-
-class GridLayer(QgsPluginLayer):
-    def __init__(self):
-        QgsPluginLayer.__init__(self, "GridLayer", "Grid Layer")
-        self.setValid(True)
--- qt_to_py.py
+++
@@ -1,8 +0,0 @@
-# script para compilar forms qt tipo *.ui par *.py
-
-from PyQt4 import uic
-fin = open('FormNewDatabase.ui','r')
-fout = open('FormNewDatabase.py','w')
-uic.compileUi(fin,fout,execute=False)
-fin.close()
-fout.close()
